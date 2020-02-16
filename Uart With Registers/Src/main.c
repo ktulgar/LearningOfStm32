6d2ba285a248578a84facc52a9dac46fa5a57f5a
Kazım Tulgaroğlu <46278069+ktulgar@users.noreply.github.com>
2020-02-16 19:44:25 +0300

    Add files via upload

---
+++ Uart With Registers/Src/main.c
@@ -0,0 +1,192 @@
+
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+void initLEDS(void);
+void initUART(void);
+
+void receiveMessage(char[] , int);
+char receiveChar(void);
+
+void sendMessage(char[] , int);
+void sendChar(char);
+
+char txBuffer[50];
+char rxBuffer[50];
+
+int main(void)
+{
+  
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+	initLEDS();
+	initUART();
+  
+ 
+  while (1)
+		
+  {
+	
+		
+	  	/*  Receiving Char
+		
+		char message = receiveChar();
+		
+		if(message == 'g') 
+			   GPIOD->ODR = 0x1000; 
+		else if(message == 'o') 
+			   GPIOD->ODR = 0x2000; 
+		else if(message == 'r') 
+			   GPIOD->ODR = 0x4000; 
+		else if(message == 'b') 
+			   GPIOD->ODR = 0x8000; 
+		
+	   */
+	
+		
+		
+		
+		/* Sending Message
+		
+		sendMessage(txBuffer,sprintf(txBuffer,"Hello World\n"));
+		HAL_Delay(100000); 
+		
+		*/
+		
+	
+  }
+ 
+}
+
+void initLEDS(void) {
+
+	RCC->AHB1ENR |= 0x8;
+	GPIOD->MODER |= 0x55000000;
+	GPIOD->OSPEEDR |=  0xFF000000;
+	GPIOD->PUPDR = 0 ;
+	
+}
+
+void initUART(void) {
+
+	RCC->APB1ENR |= 0x80000;
+	RCC->AHB1ENR |= 0x1;
+	
+	GPIOA->MODER |= 0xA;
+	GPIOA->OTYPER |= 0x3;
+	GPIOA->AFR[0] |= 0x88;
+	
+	UART4->CR1 |= 0x200C; 
+	UART4->BRR = 0x1117;
+
+}
+
+void receiveMessage(char rxBuffer[] , int length) {
+	
+	for(int i=0 ; i<length ; i++) {
+	
+		*rxBuffer = receiveChar();
+     rxBuffer++;		
+	
+	}
+		
+}
+
+char receiveChar(void) {
+
+	char message;
+	while(!(UART4->SR & USART_SR_RXNE));		
+	message = UART4->DR;
+	return message; 
+
+}
+
+void sendMessage(char message[] ,int length) {
+
+  for(int i=0 ; i<length ; i++) {
+	
+		sendChar(*message);
+		message++;
+	
+	}
+
+}
+
+void sendChar(char myChar) {
+
+	while(!(UART4->SR & USART_SR_TXE));
+	UART4->DR = myChar;
+	while(!(UART4->SR & USART_SR_TC));
+
+}
+
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+ 
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+ 
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+ 
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+static void MX_GPIO_Init(void)
+{
+
+
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+
+void Error_Handler(void)
+{
+  
+}
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif 
